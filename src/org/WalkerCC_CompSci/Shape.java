5433084e4eed6980839d73ffd62f9db5f2b68e53
priceoli000 <priceoli000@warren.k12.in.us>
2022-02-25 12:43:43 -0500

    Finished AbstractShape

--- src/org/WalkerCC_CompSci/Shape.java
+++ src/org/WalkerCC_CompSci/Shape.java
@@ -18,10 +18,37 @@ public abstract class Shape {
 
     private double CalculateSideLength(Point point0, Point point1){
         //Does the GetSideLength math work stuff
+
+        double x0 = point0.getX();
+        double y0 = point0.getY();
+        double x1 = point1.getX();
+        double y1 = point1.getY();
+        double xDist = x0 - x1;
+        double yDist = y0 - y1;
+        double xTotal = Math.pow(xDist, 2);
+        double yTotal = Math.pow(yDist, 2);
+        double yAndX = xTotal + yTotal;
+        return Math.sqrt(yAndX);
+
+
+
     }
 
     public double GetPerimeter(){
         //Does the Perimeter Math Work Stuff
+        double total = 0;
+        for (int i = 0; i < listOfPoints.length; i++) {
+
+            total = 0;
+            total = total + GetSideLength(i);
+            total = total + GetSideLength(i);
+            total = total + GetSideLength(i);
+            total = total + GetSideLength(i);
+
+
+        }
+
+        return total;
     }
 
 
